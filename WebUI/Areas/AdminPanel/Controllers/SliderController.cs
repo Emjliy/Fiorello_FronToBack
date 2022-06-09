f9d8806a053980d4818367c79fe14427e16eb18f
Emjliy <6p304s8@code.edu.az>
2022-06-09 08:30:35 +0400

    Guid+Filestream

--- WebUI/Areas/AdminPanel/Controllers/SliderController.cs
+++ WebUI/Areas/AdminPanel/Controllers/SliderController.cs
@@ -39,53 +39,19 @@ namespace WebUI.Areas.AdminPanel.Controllers
                 ModelState.AddModelError("Photo", "max size must be  less than 200kb");
                 return View();
             }
-
             if (!(slide.Photo.ContentType.Contains("image/")))
             {
                 ModelState.AddModelError("Photo", "type file must be image");
                 return View();
             }
+            var fileName = Guid.NewGuid().ToString() + slide.Photo.FileName;
+
             using (
-                FileStream filestream = new FileStream(@"C: \Users\Emilia\Desktop\Fiorella\img\" + slide.Photo.FileName, FileMode.Create))
+                FileStream filestream = new FileStream(@"C:\Users\Emilia\Desktop\Fiorella\img\" + fileName, FileMode.Create))
             {
                 slide.Photo.CopyTo(filestream);
             }
-                return Json(slide.Photo.FileName);
-
+            return Json(fileName);
         }
-        //[HttpPost]
-
-        //[ValidateAntiForgeryToken]
-        //public async Task<IActionResult> Upload(int? id, Slider slide)
-        //{
-        //    if (id == null)
-        //    {
-        //        return BadRequest();
-        //    }
-        //    Slider slideDb = _context.Slides.FirstOrDefault(s => s.ID == id);
-        //    if (slide == null)
-        //    {
-        //        return NotFound();
-        //    }
-        //    if (slide.Photo.FileName == slideDb.Photo.FileName)
-        //    {
-        //        return RedirectToAction(nameof(Index));
-        //    }
-        //    else
-        //    {
-
-        //    }
-        //    bool isExist = Slider.Any(s => s.pho.ToLower() == slide.Photo.FileName.ToLower());
-        //    if (isExist)
-        //    {
-        //        ModelState.AddModelError("Photo", $"{slide.Photo.FileName} is exist.");
-        //        return View();
-        //    }
-        //    slideDb.Photo.FileName = slide.Photo.FileName;
-        //    await _context.SaveChangesAsync();
-        //    return RedirectToAction(nameof(Index));
-
-        //}
-
     }
 }
